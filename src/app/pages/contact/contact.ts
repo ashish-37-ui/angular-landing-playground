537c63d167bfb658cbf01f76db4dd15d5bca2e0d
ashish-me-15 <ashish.sharma75.email@gmail.com>
2026-02-10 12:36:09 +0530

    Added contact page & Forms and validation

---
+++ src/app/pages/contact/contact.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [ReactiveFormsModule, NgIf],
+  templateUrl: './contact.html',
+  styleUrls: ['./contact.css']
+})
+export class ContactComponent {
+
+  contactForm;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  submit() {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Form Submitted:', this.contactForm.value);
+    this.contactForm.reset();
+  }
+}
